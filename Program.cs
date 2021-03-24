34748ff076748a8d162f81fcbf92087791606f51
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-03-23 20:10:22 -0600

    attempting a while Loop

--- Program.cs
+++ Program.cs
@@ -2,11 +2,29 @@
 
 namespace BinarySearch_1
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      int max = 10;
+      int min = 1;
+      int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+      Console.WriteLine("Guess which number is correct");
+      Console.WriteLine("1, 2, 3, 4, 5, 6, 7, 8, 9, 10");
+      int userChoice = Console.ReadLine();
+
+      while (userChoise != 8)
+      {
+        if (userChoise < 8)
         {
-            Console.WriteLine("Hello World!");
+
         }
+        else
+        {
+
+        }
+        Console.WriteLine("End of script")
+      }
     }
+  }
 }
