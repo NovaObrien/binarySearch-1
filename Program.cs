1c86f09cb4467d43ac903f483d4709a1cf5208af
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-03-23 20:37:55 -0600

    kinda works idea is shown...

--- Program.cs
+++ Program.cs
@@ -6,24 +6,35 @@ namespace BinarySearch_1
   {
     static void Main(string[] args)
     {
-      int max = 10;
-      int min = 1;
+      int target = 8;
+      int guess = 0;
+      int max = target - 1;
+      int min = 0;
       int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-      Console.WriteLine("Guess which number is correct");
-      Console.WriteLine("1, 2, 3, 4, 5, 6, 7, 8, 9, 10");
-      int userChoice = Console.ReadLine();
 
-      while (userChoise != 8)
+      while (guess != target)
       {
-        if (userChoise < 8)
+        guess = max - min / 2;
+        if (max < min)
         {
-
+          Console.WriteLine("Target is not present");
+          break;
+        }
+        else if (nums[guess] == target)
+        {
+          Console.WriteLine("Found Target is equal to", target);
+          break;
+        }
+        else if (nums[guess] < target)
+        {
+          min = guess + 1;
+          Console.WriteLine("Guess is to Low", target);
         }
         else
         {
-
+          max = guess - 1;
+          Console.WriteLine("Guess is to High", target);
         }
-        Console.WriteLine("End of script")
       }
     }
   }
