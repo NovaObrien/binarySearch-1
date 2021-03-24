8e309fc185f437da494c35bf369df9bdb20f395f
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-03-23 19:54:53 -0600

    created simple hello world

--- Program.cs
+++ Program.cs
@@ -1,29 +1,12 @@
-// Let min = 1min=1m, i, n, equals, 1 and max = nmax=nm, a, x, equals, n.
-// Guess the average of maxmaxm, a, x and minminm, i, n, rounded down so that it is an integer.
-// If you guessed the number, stop. You found it!
-// If the guess was too low, set minminm, i, n to be one larger than the guess.
-// If the guess was too high, set maxmaxm, a, x to be one smaller than the guess.
-// Go back to step two.
-// let min = 1;
-// let max = 24;
-// let primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+﻿using System;
 
-// function binarySearch()
-// {
-//   while (userChoise != computerChoice)
-//   {
-
-//   }
-// }
-
-using System;
 namespace BinarySearch_1
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Hello Wolrd!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
     }
-  }
 }
