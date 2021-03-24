ca05797a238b7a4a586e9f7e409d1daaf6a1899f
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-03-23 21:27:16 -0600

    Not working as predicted

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace BinarySearch_1
     {
       int target = 8;
       int guess = 0;
-      int max = target - 1;
+      int max = 9;
       //   TODO Max is wrong
       int min = 0;
       int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
@@ -23,18 +23,18 @@ namespace BinarySearch_1
         }
         else if (nums[guess] == target)
         {
-          Console.WriteLine("Found Target is equal to" + target);
+          Console.WriteLine("Found Target is equal to " + guess);
           break;
         }
         else if (nums[guess] < target)
         {
           min = guess + 1;
-          Console.WriteLine("Guess is to Low" + target);
+          Console.WriteLine("Guess is to Low " + guess);
         }
         else
         {
           max = guess - 1;
-          Console.WriteLine("Guess is to High", target);
+          Console.WriteLine("Guess is to High " + guess);
         }
       }
     }
